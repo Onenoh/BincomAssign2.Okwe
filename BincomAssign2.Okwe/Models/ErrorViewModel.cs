638696e570eb7a3667acb4a4cc15654849f44bd0
Anthonia Okwe <anthonia_okwe@yahoo.com>
2024-10-10 23:55:31 +0100

    Add project files.

---
+++ BincomAssign2.Okwe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BincomAssign2.Okwe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
